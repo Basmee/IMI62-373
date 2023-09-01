b21b461efdd406a785b5017fc5b5e3a7045fc5a2
Basmee <110513437+Basmee@users.noreply.github.com>
2023-09-02 00:46:47 +0700

    Update config.dart

--- lib/screens/config.dart
+++ lib/screens/config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_application_17/screens/users.dart';
 
 class Configure {
-  static const server = "10.116.3.64:3000";
+  static const server = "192.168.1.145:3000";
   static Users login = Users();
   static List<String> gender = [
     "None",
